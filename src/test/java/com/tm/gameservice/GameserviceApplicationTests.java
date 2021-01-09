3dcd0ec94613def5b9cf65c460b4ab8cd13d682e
monroeshindelar <monroeshindelar@gmail.com>
2021-01-08 17:39:32 -0800

    Initial Commit

---
+++ src/test/java/com/tm/gameservice/GameserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
